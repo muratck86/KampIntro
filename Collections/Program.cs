e9a0728e8699f717cf28e731fd06d6e551d7fc6d
Murat Can Kurt <59605826+muratck86@users.noreply.github.com>
2021-01-21 03:43:23 +0300

    Added collections and OOP1 projects to the solution

---
+++ Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names2 = new List<string> { "Mahmut", "Ahmet", "Sencer", "Ökkeş", "Cebbar" };
+            names2.Add("İlker");
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[5]);
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 8
+            product1.CategoryId = 5;
+            product1.ProductName = "Çokomel";
+            product1.UnitPrice = 2.75;
+            product1.UnitsInStock = 54;
+
+            Product product2 = new Product
+            {
+                Id = 8,
+                CategoryId = 5,
+                ProductName = "GofGof Gofret",
+                UnitPrice = 1.25,
+                UnitsInStock = 126
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+        }
+    }
+}
